bcdc63c8a025a44fb098dcc6f75bee19dc029b0f
Swadhin <swadhinghosh31@gmail.com>
2023-01-24 03:14:46 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-//your code here
+
+const images = document.querySelectorAll("img");
+const h3 = document.getElementById("h");
+const resetBtn = document.getElementById("reset");
+const verifyBtn = document.getElementById("verify");
+const para = document.getElementById("para");
+
+
+let selectedImages = [];
+
+
+images.forEach(function(image) {
+    image.addEventListener("click", function() {
+       
+        selectedImages.push(image);
+
+        
+        if (selectedImages.length === 2) {
+            verifyBtn.style.display = "block";
+        }
+        if (selectedImages.length > 2) {
+            selectedImages.shift();
+        }
+    });
+});
+
+
+verifyBtn.addEventListener("click", function() {
+    if (selectedImages[0].className === selectedImages[1].className) {
+        para.innerHTML = "You are a human. Congratulations!";
+        para.style.display = "block";
+    } else {
+        para.innerHTML = "We can't verify you as a human. You selected the non-identical tiles.";
+        para.style.display = "block";
+    }
+    verifyBtn.style.display = "none";
+    resetBtn.style.display = "block";
+});
+
+resetBtn.addEventListener("click", function() {
+    selectedImages = [];
+    resetBtn.style.display = "none";
+    para.style.display = "none";
+});
